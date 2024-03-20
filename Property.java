78d00c019d6dee38257a84cd0aac9ff32e5f48f5
DavidF67 <162385293+DavidF67@users.noreply.github.com>
2024-03-20 11:02:05 +0000

    Nivel 1

--- Property.java
+++ Property.java
@@ -9,7 +9,7 @@
 public class Property {
 
     private static int numberOfProperties = 0;
-    private final String id;
+    private final int id;
     private String description;
     private double price;
 
@@ -20,21 +20,23 @@ public class Property {
      * @param price The property price.
      */
     public Property(String description, double price) {
-
+        id = numberOfProperties + 1;
+        this.description = description;
+        this.price = price;
     }
 
     /**
      * Id selector.
      */
     public int getId() {
-        
+        return id;
     }
 
     /**
      * Description selector.
      */
     public String getDescription() {
-
+        return description;
     }
 
     /**
@@ -43,14 +45,14 @@ public class Property {
      * @param description The new description. Must not be null.
      */
     public void setDescription(String description) {
-
+        this.description = description;
     }
 
     /**
      * Price selector.
      */
     public double getPrice() {
-        
+        return price;
     }
     
     /**
@@ -59,10 +61,15 @@ public class Property {
      * @param price The new price. Must not be negative.
      */
     public void setPrice(double price) {
-
+        this.price = price;
     }
 
     @Override
     public String toString() {
+        return "property"+id;
+    }
+    
+    public void printData(){
+        System.out.println(getId() +" " + getDescription() +" " + price);
     }
 }
---
+++ PropertyTest.java
@@ -0,0 +1,33 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class PropertyTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class PropertyTest
+{
+    
+    Property property;
+    /**
+     * Default constructor for test class PropertyTest
+     */
+    public PropertyTest()
+    {
+        
+    }
+    
+    
+    @Test
+    public void testConstructor(){
+        property = new Property("T3 Monte Belo", 150000.0);
+        assertEquals(150000.0, property.getPrice());
+        assertEquals("T3 Monte Belo", property.getDescription());
+    }
+}
